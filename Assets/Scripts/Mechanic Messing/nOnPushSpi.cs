cddcd09849190d4ee465cb20744ec855cc8b2721
ibnakhal <ibznakhal@gmail.com>
2016-09-28 13:29:38 -0700

    test interfaces

---
+++ Assets/Scripts/Mechanic Messing/nOnPushSpi.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nOnPushSpi : MonoBehaviour {
+    public Vector3 OneVector;
+    private bool moving;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(!moving)
+        {
+            this.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Rigidbody>().freezeRotation = false;
+
+        }
+
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+        {
+            moving = true;
+            this.gameObject.GetComponent<Rigidbody>().AddTorque(OneVector);
+        }
+        if (Input.touchCount<=1 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            moving = false;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Mechanic Messing/pinchZoom.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pinchZoom : MonoBehaviour
+{
+    public float pZoomSpeed = .5f;
+    public float oZoomSpeed = 0.5f;
+    Camera curcam;
+    public void Start()
+    {
+        curcam = Camera.current;
+    }
+    void Update()
+    {
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            if(curcam.orthographic)
+            {
+                curcam.orthographicSize += deltaMagDiff * oZoomSpeed;
+                curcam.orthographicSize = Mathf.Max(curcam.orthographicSize, .1f);
+            }
+            else
+            {
+                curcam.fieldOfView += deltaMagDiff * pZoomSpeed;
+                curcam.fieldOfView = Mathf.Clamp(curcam.fieldOfView, .1f, 179.9f);
+            }
+        }
+    }
+
+
+
+
+}
